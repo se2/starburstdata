d8487d5508037621a016011b7369eaedf56c622c
Tu Luong <tu@delindesign.com>
2018-06-07 18:26:45 -0400

    Addjust homepage height for desktop

--- themes/starburstdata-theme/templates/template-home.php
+++ themes/starburstdata-theme/templates/template-home.php
@@ -1,36 +1,49 @@
 <?php
-/*
-Template Name: Home
-*/
+/**
+ * Template Name: Home
+ *
+ * @category   Template
+ * @package    WordPress
+ * @subpackage StarburstData
+ * @author     Delin Design <contact@delindesign.com>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://delindesign.com
+ */
 
-get_header(); ?>
+get_header();
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+?>
 
-<div class="bg-cover home-banner-background" style="background-image: url(<?php the_field('home_banner_image'); ?>;">
+<div class="bg-contain bg-center-right home-banner pos-rel" style="background-image: url(<?php the_field( 'home_banner_image' ); ?>;">
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x">
-			<div class="home-banner-container cell medium-4">
-				<h1 class="home-banner-headline"><?php the_field('home_banner_headline'); ?></h1>
-				<h3 class="home-banner-subhead"><?php the_field('home_banner_subhead'); ?></h3>
+			<div class="home-banner-container cell medium-5">
+				<h1 class="home-banner-headline"><?php the_field( 'home_banner_headline' ); ?></h1>
+				<h6 class="secondary-color"><?php the_field( 'home_banner_subhead' ); ?></h6>
 			</div>
 		</div>
 	</div>
 </div>
 
 <div class="grid-container text-center main-container">
-	<?php if( have_rows('home_intro') ): ?>
-		<div class="grid-x grid-padding-x medium-up-3 small-up-1">
-			<?php while( have_rows('home_intro') ): the_row();
-				$home_intro_icon = get_sub_field('home_intro_icon');
-				$home_intro_header = get_sub_field('home_intro_header');
-				$home_intro_description = get_sub_field('home_intro_description');
-				?>
-				<div class="cell">
-					<img class="home-intro-icon" src="<?php echo $home_intro_icon['url']; ?>" alt="<?php echo $home_intro_icon['alt']; ?>" />
-					<h4 class="home-intro-header"><?php echo $home_intro_header; ?></h4>
-					<p class="home-intro-description"><?php echo $home_intro_description; ?></p>
-				</div>
-			<?php endwhile; ?>
+	<?php if ( have_rows( 'home_intro' ) ) : ?>
+	<div class="grid-x grid-padding-x medium-up-3 small-up-1">
+		<?php
+		while ( have_rows( 'home_intro' ) ) :
+			the_row();
+			$home_intro_icon        = get_sub_field( 'home_intro_icon' );
+			$home_intro_header      = get_sub_field( 'home_intro_header' );
+			$home_intro_description = get_sub_field( 'home_intro_description' );
+		?>
+		<div class="cell">
+			<img class="home-intro-icon" src="<?php echo esc_attr( $home_intro_icon['url'] ); ?>" alt="<?php echo esc_attr( $home_intro_icon['alt'] ); ?>" />
+			<h4 class="home-intro-header"><?php the_sub_field( 'home_intro_header' ); ?></h4>
+			<p class="home-intro-description"><?php the_sub_field( 'home_intro_description' ); ?></p>
 		</div>
+		<?php endwhile; ?>
+	</div>
 	<?php endif; ?>
 </div>
 
@@ -38,11 +51,15 @@ get_header(); ?>
 	<div class="grid-container">
 		<div class="grid-x grid-padding-x">
 			<div class="cell">
-				<a class="button reverse" href="<?php the_field('home_cta_link'); ?>" target="_blank"><?php the_field('home_cta_text'); ?></a>
+				<a class="button reverse" href="<?php the_field( 'home_cta_link' ); ?>" target="_blank"><?php the_field( 'home_cta_text' ); ?></a>
 			</div>
 		</div>
 	</div>
 </div>
 
+<?php
+	endwhile;
+endif;
+?>
 
 <?php get_footer(); ?>
--- themes/starburstdata-theme/templates/template-leadership.php
+++ themes/starburstdata-theme/templates/template-leadership.php
@@ -1,17 +1,29 @@
 <?php
-/*
-Template Name: Leadership
-*/
+/**
+ * Template Name: Leadership
+ *
+ * @category   Template
+ * @package    WordPress
+ * @subpackage StarburstData
+ * @author     Delin Design <contact@delindesign.com>
+ * @license    https://www.gnu.org/licenses/gpl-3.0.txt GNU/GPLv3
+ * @link       https://delindesign.com
+ */
 
-get_header(); ?>
-			
+get_header();
+
+if ( have_posts() ) :
+	while ( have_posts() ) :
+		the_post();
+?>
+
 <?php get_template_part( 'parts/content', 'banner' ); ?>
 
 <div class="grid-container main-container text-center">
 	<div class="grid-x grid-padding-x align-center">
 		<div class="cell medium-10 small-12">
-			<h3><?php the_field('leadership_intro_header'); ?></h3>
-			<?php the_field('leadership_intro_content'); ?>
+			<h3><?php the_field( 'leadership_intro_header' ); ?></h3>
+			<?php the_field( 'leadership_intro_content' ); ?>
 		</div>
 	</div>
 </div>
@@ -19,27 +31,33 @@ get_header(); ?>
 <div class="grid-container">
 	<div class="grid-x grid-padding-x align-center">
 		<div class="cell medium-10 small-12">
-				<?php if( have_rows('leaders_repeater') ): ?>
-					<?php while( have_rows('leaders_repeater') ): the_row(); 
-						$leaders_headshot = get_sub_field('leaders_headshot');
-						$leaders_name = get_sub_field('leaders_name');
-						$leaders_description = get_sub_field('leaders_description');
-						?>
-						<div class="grid-x grid-padding-x leader-container">
-							<div class="cell medium-3 small-12 leader-headshot">
-								<img src="<?php echo $leaders_headshot['url']; ?>" alt="<?php echo $leaders_headshot['alt']; ?>" />
-							</div>
-
-							<div class="cell medium-9 small-12">
-								<h3 class="leader-name"><?php echo $leaders_name; ?></h3>
-								<span class="leader-description"><?php echo $leaders_description; ?></span>
-							</div>
+				<?php
+				if ( have_rows( 'leaders_repeater' ) ) :
+					while ( have_rows( 'leaders_repeater' ) ) :
+						the_row();
+						$leaders_headshot    = get_sub_field( 'leaders_headshot' );
+						$leaders_name        = get_sub_field( 'leaders_name' );
+						$leaders_description = get_sub_field( 'leaders_description' );
+				?>
+					<div class="grid-x grid-padding-x leader-container">
+						<div class="cell medium-3 small-12 leader-headshot">
+							<img src="<?php echo esc_attr( $leaders_headshot['url'] ); ?>" alt="<?php echo esc_attr( $leaders_headshot['alt'] ); ?>" />
 						</div>
-					<?php endwhile; ?>
-				<?php endif; ?>
+
+						<div class="cell medium-9 small-12">
+							<h3 class="leader-name"><?php the_sub_field( 'leaders_name' ); ?></h3>
+							<span class="leader-description"><?php the_sub_field( 'leaders_description' ); ?></span>
+						</div>
+					</div>
+				<?php endwhile; ?>
+			<?php endif; ?>
 		</div>
 	</div>
 </div>
 
+<?php
+	endwhile;
+endif;
+?>
 
 <?php get_footer(); ?>
