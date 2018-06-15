a582e375df0f5e27ec7171f9f75d00f687d0ecd1
Tu Luong <tu@delindesign.com>
2018-06-15 15:43:23 -0400

    Update header scripts

--- themes/starburstdata-theme/header.php
+++ themes/starburstdata-theme/header.php
@@ -42,6 +42,46 @@
 
 		<?php wp_head(); ?>
 
+		<!-- Start of Async Drift Code -->
+		<script>
+		!function() {
+			var t;
+			if (t = window.driftt = window.drift = window.driftt || [], !t.init) return t.invoked ? void (window.console && console.error && console.error("Drift snippet included twice.")) : (t.invoked = !0,
+			t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+			t.factory = function(e) {
+				return function() {
+					var n;
+					return n = Array.prototype.slice.call(arguments), n.unshift(e), t.push(n), t;
+				};
+			}, t.methods.forEach(function(e) {
+				t[e] = t.factory(e);
+			}), t.load = function(t) {
+				var e, n, o, i;
+				e = 3e5, i = Math.ceil(new Date() / e) * e, o = document.createElement("script"),
+				o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + i + "/" + t + ".js",
+				n = document.getElementsByTagName("script")[0], n.parentNode.insertBefore(o, n);
+			});
+		}();
+		drift.SNIPPET_VERSION = '0.3.1';
+		drift.load('rxck2emr6rp6');
+		</script>
+		<!-- End of Async Drift Code -->
+
+		<!-- Global site tag (gtag.js) - Google AdWords: 815997738 -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=AW-815997738"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'AW-815997738');
+		</script>
+
+		<!-- Start of starburstdata Zendesk Widget script
+		<script>/*<![CDATA[*/window.zEmbed||function(e,t){var n,o,d,i,s,a=[],r=document.createElement("iframe");window.zEmbed=function(){a.push(arguments)},window.zE=window.zE||window.zEmbed,r.src="javascript:false",r.title="",r.role="presentation",(r.frameElement||r).style.cssText="display: none",d=document.getElementsByTagName("script"),d=d[d.length-1],d.parentNode.insertBefore(r,d),i=r.contentWindow,s=i.document;try{o=s}catch(e){n=document.domain,r.src='javascript:var d=document.open();d.domain="'+n+'";void(0);',o=s}o.open()._l=function(){var e=this.createElement("script");n&&(this.domain=n),e.id="js-iframe-async",e.src="https://assets.zendesk.com/embeddable_framework/main.js",this.t=+new Date,this.zendeskHost="starburstdata.zendesk.com",this.zEQueue=a,this.body.appendChild(e)},o.write('<body onload="document._l();">'),o.close()}();
+		/*]]>*/</script>
+		End of starburstdata Zendesk Widget script -->
+
 	</head>
 
 	<body <?php body_class(); ?>>
